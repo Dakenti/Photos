d00cafa5a320063485b194a25f34547368981ef1
Darkhan <didar3@mail.ru>
2021-08-30 18:26:49 +0600

    Initial Commit

---
+++ Photos/PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosApp.swift
+//  Photos
+//
+//  Created by Serkeshev Darkhan on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
